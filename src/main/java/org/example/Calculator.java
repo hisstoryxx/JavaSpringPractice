b8203acdafd689bf05b60e5911445640c03c6720
cho <hisstoryxx@gmail.com>
2023-05-10 17:58:59 +0900

    + refactoring passed

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Calculator {
     public static int calculate(int operand1, String operator, int operand2) {
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
 
-        return operand1 + operand2;
+        }
+        return 0;
+
     }
 }
